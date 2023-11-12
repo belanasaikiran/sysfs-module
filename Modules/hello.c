39ed78e3672a680b1294b3cac53527b4eabe1260
Saikiran Belana <88756154+belanasaikiran@users.noreply.github.com>
2023-11-12 16:36:27 +0000

    add hello module

---
+++ Modules/hello.c
@@ -0,0 +1,16 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+static int hello_init(void){
+	printk(KERN_ALERT "TEST: Hello world, this is Sanju\n");
+	return 0;
+}
+
+
+static void hello_exit(void){
+	printk(KERN_ALERT "TEST: Good byte, from Sanju\n");
+}
+
+
+module_init(hello_init);
+module_exit(hello_exit);
