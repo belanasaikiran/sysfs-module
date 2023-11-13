5592743012eb30cfa688cab7d58e0c3bc91f5c55
Sai Kiran Belana <saikiransanju22@outlook.com>
2023-11-13 22:17:39 +0530

    add hello module :: TEST

--- Modules/hello.c
+++ Modules/hello.c
@@ -1,6 +1,12 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Sai Kiran Belana");
+MODULE_DESCRIPTION("Hello Kernel Module to test");
+
+
 static int hello_init(void){
 	printk(KERN_ALERT "TEST: Hello world, this is Sanju\n");
 	return 0;
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # sysfs-module
+
+
+1. Install the linux headers
+```
+sudo apt install linux-headers-$(uname -r)
+```
+
+
+2. Also install linux-source to correct the error of `make menuconfig`
+
